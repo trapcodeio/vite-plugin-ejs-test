87044ce401627fbe4751017103c1f7b0c8d06a06
trapcodeio <48396844+trapcodeio@users.noreply.github.com>
2022-11-13 08:47:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vite-plugin-ejs-test
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vite-plugin-ejs)
---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { ViteEjsPlugin } from 'vite-plugin-ejs';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+
+    ViteEjsPlugin(
+      () => {
+        return {
+          title: 'My vue project!',
+        };
+      },
+      {
+        ejs: {
+          // set views path to support partials
+          views: [__dirname],
+        },
+      }
+    ),
+  ],
+});
